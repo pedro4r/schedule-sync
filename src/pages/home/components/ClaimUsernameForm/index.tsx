fcf759433148d5fb2031c523d02d9a9e1a2bb59c
Pedro Requiao <alonsofts@gmail.com>
2023-08-16 18:32:15 -0400

    add: ZodResolver at Claim Username Form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,19 +1,57 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { Form, FormAnnotation } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+    username: z
+        .string()
+        .min(3, { message: 'Username must have at least 3 letters' })
+        .regex(/^([a-z\\-]+)$/i, {
+            message: 'Username must have only letter and hyphen',
+        })
+        .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ClaimUsernameFormData>({
+        resolver: zodResolver(claimUsernameFormSchema),
+    })
+
+    async function handleClaimUsername(data: ClaimUsernameFormData) {
+        console.log(data)
+    }
+
     return (
-        <Form as='form'>
-            <TextInput
-                size='sm'
-                prefix='ignite.com/'
-                placeholder='seu-usuário'
-            />
-            <Button size='sm' type='submit'>
-                Reservar
-                <ArrowRight />
-            </Button>
-        </Form>
+        <>
+            <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
+                <TextInput
+                    size='sm'
+                    prefix='ignite.com/'
+                    placeholder='seu-usuário'
+                    {...register('username')}
+                />
+                <Button size='sm' type='submit'>
+                    Claim
+                    <ArrowRight />
+                </Button>
+            </Form>
+
+            <FormAnnotation>
+                <Text size='sm'>
+                    {errors.username
+                        ? errors.username.message
+                        : 'Type your username'}
+                </Text>
+            </FormAnnotation>
+        </>
     )
 }
--- src/pages/home/components/ClaimUsernameForm/styles.ts
+++ src/pages/home/components/ClaimUsernameForm/styles.ts
@@ -1,4 +1,4 @@
-import { styled, Box } from '@ignite-ui/react'
+import { styled, Box, Text } from '@ignite-ui/react'
 
 export const Form = styled(Box, {
     display: 'grid',
@@ -10,4 +10,12 @@ export const Form = styled(Box, {
     '@media(max-width: 600px)': {
         gridTemplateColumns: '1fr',
     },
+})
+
+export const FormAnnotation = styled('div', {
+    marginTop: '$2',
+
+    [`> ${Text}`]: {
+        color: '$gray400'
+    }
 })
