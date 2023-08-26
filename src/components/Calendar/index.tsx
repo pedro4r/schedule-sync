682619217f738d0f8ae7ec73003d2e5032d80076
Pedro Requiao <alonsofts@gmail.com>
2023-08-26 15:04:58 -0400

    feat: Calendar constructor formula

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,7 +8,7 @@ import {
     CalendarHeader,
     CalendarTitle,
 } from './styles'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
 export function Calendar() {
@@ -30,6 +30,27 @@ export function Calendar() {
 
     const currentMonth = currentDate.format('MMMM')
     const currentYear = currentDate.format('YYYY')
+
+    const calendarWeeks = useMemo(() => {
+        const daysInMonthArray = Array.from({
+            length: currentDate.daysInMonth(),
+        }).map((_, i) => {
+            return currentDate.set('date', i + 1)
+        })
+
+        const firstWeekDay = currentDate.get('day')
+
+        const previousMonthFillArray = Array.from({
+            length: firstWeekDay,
+        })
+            .map((_, i) => {
+                console.log(currentDate.subtract(i + 1, 'day'))
+                return currentDate.subtract(i + 1, 'day')
+            })
+            .reverse()
+
+        return daysInMonthArray
+    }, [currentDate])
 
     return (
         <CalendarContainer>
